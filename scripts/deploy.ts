b510797cee649263412f5783e863e396a6ca124e
Arano <aranoverse@outlook.com>
2022-11-23 18:23:10 +0800

    chore: mock call

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,29 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import { upgrade } from "./proxy-admin/proxy-admin-common";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  //
+  // const lockedAmount = ethers.utils.parseEther("1");
+  //
+  // const Lock = await ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  //
+  // await lock.deployed();
+  //
+  // console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const TransparentUpgradeableProxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
+  const VotableProxyAdmin = await ethers.getContractFactory("VotableProxyAdmin");
+  const MockProposerRegistry = await ethers.getContractFactory("MockProposerRegistry");
+  const registry = await MockProposerRegistry.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // await registry.grantProposer();
 
-  await lock.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
